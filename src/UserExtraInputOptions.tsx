cdb47063f07d8c8c97e688a6dab85f2223a1573a
Thomas_Ju <yuanping@umich.edu>
2023-03-07 22:38:12 -0800

    finish infowindow in google map

--- src/UserExtraInputOptions.tsx
+++ src/UserExtraInputOptions.tsx
@@ -10,10 +10,40 @@ export default function UserExtraInputOptions() {
     e.preventDefault();
   }
 
+
   return (
-    <Form onSubmit = {clickGenerateItinerary }>
+    <Form onSubmit={clickGenerateItinerary}>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridDays">
+          <Form.Label>How many days is your trip?</Form.Label>
+          <Form.Control type="days" placeholder="3" />
+        </Form.Group>
+      </Row>
+
+      <Row className="mb-3">
+        {/* https://developers.google.com/maps/documentation/places/web-service/supported_types */}
+        <Form.Group as={Col} controlId="formGridStartTime">
+          <Form.Label>StartTime</Form.Label>
+          <Form.Control type="time" placeholder="09:00" />
+        </Form.Group>
+        <Form.Group as={Col} controlId="formGridEndTime">
+          <Form.Label>EndTime</Form.Label>
+          <Form.Control type="time" placeholder="17:00" />
+        </Form.Group>
+      </Row>
+
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridtype">
+          <Form.Label>Transportation</Form.Label>
+          <Form.Select defaultValue="Public Transportation">
+            <option>Public Transportation</option>
+            <option>Car</option>
+          </Form.Select>
+        </Form.Group>
+      </Row>
+
       <Button variant="primary" type="submit">
-        Generate Itinerary 
+          Generate Itinerary 
       </Button>
     </Form>
   );
